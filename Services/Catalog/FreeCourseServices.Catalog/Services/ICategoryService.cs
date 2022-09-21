29149f529acb25a6d35a9fc2a9e379b0943753ba
enes.cicek <enes.cicek@vbt.com.tr>
2022-09-21 17:31:27 +0300

    Microservices added

---
+++ Services/Catalog/FreeCourseServices.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourseServices.Catalog.Dtos;
+using FreeCourseServices.Catalog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourseServices.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        public Task<Response<List<CategoryDto>>> GetAllAsync();
+        public Task<Response<CategoryDto>> CreateAsync(CategoryDto category);
+        public Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
---
+++ Services/Catalog/FreeCourseServices.Catalog/Services/ICourseService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourseServices.Catalog.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourseServices.Catalog.Services
+{
+    public interface ICourseService
+    {
+        public Task<Response<List<CourseDto>>> GetAllAsync();
+        public Task<Response<CourseDto>> GetByIdAsync(string id);
+        public Task<Response<List<CourseDto>>> GetAllByUserId(string userId);
+        public Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto);
+        public Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdate);
+        public Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
